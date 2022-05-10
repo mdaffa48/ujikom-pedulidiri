c0a1fbe522a17113692d4de8445bdd5a32b7fa8b
mdaffa48 <work@muhammaddaffa.com>
2022-05-10 22:21:33 +0700

    Add dahsboard main menu

---
+++ resources/views/pages/dashboard/main.blade.php
@@ -0,0 +1,11 @@
+@extends('pages.dashboard.dashboard')
+
+@section('title', 'Peduli Diri - Menu Utama')
+
+@section('halaman')
+    <h1>Hallo, selamat datang "{{ auth()->user()->name }}" di Peduli Diri</h1>
+@endsection
+
+@section('body')
+
+@endsection
